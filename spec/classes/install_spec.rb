e41f26854290736b6d890e7ccef0e50e1e93e67a
Martin Aamodt <martaamo@stud.ntnu.no>
2019-10-08 12:06:15 +0200

    fix linting error

--- spec/classes/install_spec.rb
+++ spec/classes/install_spec.rb
@@ -4,7 +4,7 @@ describe 'cockroachdb::install' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
-      let(:node_params) { { 'cockroachdb::dependencies' => '["tar","wget"]' } }
+      let(:node_params) {'cockroachdb::dependencies' => '["tar","wget"]'}
 
       it { is_expected.to compile }
     end
