cc6f14a670f7fdf323d1da09f43780d6ab2939d8
Martin Aamodt <martaamo@stud.ntnu.no>
2019-10-08 20:50:44 +0200

    includes cockroachdb in test

--- spec/classes/config_spec.rb
+++ spec/classes/config_spec.rb
@@ -4,6 +4,7 @@ describe 'cockroachdb::config' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
+      let(:pre_condition) { 'include cockroachdb' }
 
       it { is_expected.to compile }
     end
