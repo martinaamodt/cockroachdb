f04fa5ada1d6f095d754137ec72310f1fb1acf06
Martin Aamodt <martaamo@stud.ntnu.no>
2019-10-08 11:34:19 +0200

    install is functional

--- manifests/install.pp
+++ manifests/install.pp
@@ -7,13 +7,22 @@
 class cockroachdb::install {
   package { $cockroachdb::dependencies:
     provider => apt,
-    ensure => installed,
+    ensure   => installed,
+    before   => Download_uncompress['dwnl_inst_cockroach'],
   }
-download_uncompress {'dwnl_inst_cockroach':
-    download_base_url  => 'https://binaries.cockroachdb.com/cockroach-v19.1.5.linux-amd64.tgz',
-    distribution_name  => 'cockroach-v19.1.5.linux-amd64.tgz'
-    dest_folder   => '/tmp',
-    creates       => '/usr/local/bin/cockroach',
-    uncompress    => 'tar.gz',
+  download_uncompress {'dwnl_inst_cockroach':
+    download_base_url  => 'https://binaries.cockroachdb.com',
+    distribution_name  => 'cockroach-v19.1.5.linux-amd64.tgz',
+    dest_folder        => '/tmp',
+    creates            => '/tmp/cockroach',
+    uncompress         => 'tar.gz',
+    before             => File['/usr/local/bin/cockroach'],
+  } 
+
+  file { '/usr/local/bin/cockroach':
+    ensure => present,
+    source => '/tmp/cockroach-v19.1.5.linux-amd64/cockroach',
+    mode   => '755'
   }
+
 }
