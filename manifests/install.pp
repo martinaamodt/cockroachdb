8730b090ea10ac0a76924f811e14b3cf52aa28eb
Martin Aamodt <martin.aamodt97@gmail.com>
2019-10-08 10:56:30 +0200

    Creating install

--- manifests/install.pp
+++ manifests/install.pp
@@ -5,4 +5,15 @@
 # @example
 #   include cockroachdb::install
 class cockroachdb::install {
+  package { $cockroachdb::dependencies:
+    provider => apt,
+    ensure => installed,
+  }
+download_uncompress {'dwnl_inst_cockroach':
+    download_base_url  => 'https://binaries.cockroachdb.com/cockroach-v19.1.5.linux-amd64.tgz',
+    distribution_name  => 'cockroach-v19.1.5.linux-amd64.tgz'
+    dest_folder   => '/tmp',
+    creates       => '/usr/local/bin/cockroach',
+    uncompress    => 'tar.gz',
+  }
 }
