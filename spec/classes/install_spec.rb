7586af75c63375323ec93289a6761600a7ad64ab
Zackeus Bengtsson <zackeusb@stud.ntnu.no>
2019-10-11 13:48:20 +0200

    fix syntax error

--- spec/classes/install_spec.rb
+++ spec/classes/install_spec.rb
@@ -3,13 +3,14 @@ require 'spec_helper'
 describe 'cockroachdb::install' do
   let(:params) do
     {
-        install_path => '/usr/local/bin',
-        package_ensure => 'v19.1.5.linux-amd64',
-        node1ip => 'db0',
-        node2ip => 'db1',
-        node3ip => 'db2',
+      install_path => '/usr/local/bin',
+      package_ensure => 'v19.1.5.linux-amd64',
+      node1ip => 'db0',
+      node2ip => 'db1',
+      node3ip => 'db2',
     }
   end
+
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
