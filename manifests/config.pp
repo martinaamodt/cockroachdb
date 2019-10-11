87352d3f2ddec0b613ef5da7a35f677916ba2f90
Martin Aamodt <martaamo@stud.ntnu.no>
2019-10-11 12:44:51 +0200

    fixed linebreak

--- manifests/config.pp
+++ manifests/config.pp
@@ -32,8 +32,8 @@ class cockroachdb::config {
     'Service' => {
       'Type'             => 'notify',
       'WorkingDirectory' => $cockroachdb::workingdirectory,
-      'ExecStart'        => "/usr/local/bin/cockroach start --insecure --advertise-addr=${cockroachdb::node1ip} --join=$
-        {cockroachdb::node1ip},${cockroachdb::node2ip},${cockroachdb::node3ip} --cache=${cockroachdb::cache}
+      'ExecStart'        => "/usr/local/bin/cockroach start --insecure --advertise-addr=${::ipaddress} --join= \
+        ${cockroachdb::node1ip},${cockroachdb::node2ip},${cockroachdb::node3ip} --cache=${cockroachdb::cache} \
          --max-sql-memory=${cockroachdb::maxsqlmemory}",
       'TimeoutStopSec'   => $cockroachdb::timeoutstopsec,
       'Restart'          => $cockroachdb::restart,
