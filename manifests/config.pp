3ca265dddccf68c4e35fcf97f3e01b966cd9b110
martinaamodt <martin.aamodt97@gmail.com>
2019-10-12 21:01:41 +0200

    now using templating to create service file

--- manifests/config.pp
+++ manifests/config.pp
@@ -23,9 +23,26 @@ class cockroachdb::config {
     require => User['cockroach'],
   }
 
+  $insecurecockroachdb_hash = {
+    'description'       => $cockroachdb::description,
+    'workingdirectory'  => $cockroachdb::workingdirectory,
+    'node1ip'           => $cockroachdb::node1ip,
+    'node2ip'           => $cockroachdb::node2ip,
+    'node3ip'           => $cockroachdb::node3ip,
+    'cache'             => $cockroachdb::cache,
+    'maxsqlmemory'      => $cockroachdb::maxsqlmemory,
+    'timeoutstopsec'    => $cockroachdb::timeoutstopsec,
+    'restart'           => $cockroachdb::restart,
+    'restartsec'        => $cockroachdb::restartsec,
+    'standardoutput'    => $cockroachdb::standardoutput,
+    'standarderror'     => $cockroachdb::standarderror,
+    'syslogidentifier'  => $cockroachdb::syslogidentifier,
+    'user'              => $cockroachdb::user,
+  }
+
   file { '/etc/systemd/system/insecurecockroachdb.service':
     ensure  => file,
-    content => epp('cockroachdb/insecurecockroachdb.service.epp' ),
+    content => epp('cockroachdb/insecurecockroachdb.service.epp', $insecurecockroachdb_hash ),
     notify  => Service['insecurecockroachdb'],
   }
 
