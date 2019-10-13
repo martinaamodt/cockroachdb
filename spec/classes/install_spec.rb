21b77ea4bfb15d30648d2440219ac8f093547f05
Zackeus Bengtsson <zackeusb@stud.ntnu.no>
2019-10-13 17:34:19 +0200

    added cdb as rpe condition

--- spec/classes/install_spec.rb
+++ spec/classes/install_spec.rb
@@ -14,6 +14,7 @@ describe 'cockroachdb::install' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
+      let(:pre_condition) { 'include cockroachdb' }
 
       it { is_expected.to compile }
       # it { is_expected.to contain_package('tar') }
