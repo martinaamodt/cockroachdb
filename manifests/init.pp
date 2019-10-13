bb9d3be828f3aa16da5e89528cb43dbbb246a413
Zackeus Bengtsson <zackeusb@stud.ntnu.no>
2019-10-13 18:10:10 +0200

    added params class. moved defaults to params class from common.yaml. Made all options with defaults optional

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,37 +4,37 @@
 #
 # @example
 #   include cockroachdb
-class cockroachdb(
+class cockroachdb (
   # Would prefer to use Stdlib::absolutepath version. But it fails test with Puppet5
   #Stdlib::Absolutepath $servicepath,
-  String $servicepath,
+  Optional[String] $servicepath              = $cockroachdb::params::servicepath,
   #Stdlib::Absolutepath $workingdirectory,
-  String $workingdirectory,
+  Optional[String] $workingdirectory         = $cockroachdb::params::workingdirectory,
   # 
-  Tuple $dependencies,
-  String $description,
-  String $cache,
-  String $maxsqlmemory,
-  String $timeoutstopsec,
-  String $restart,
-  String $restartsec,
-  String $standardoutput,
-  String $standarderror,
-  String $syslogidentifier,
-  String $user,
-  String $node1ip,
-  String $node2ip,
-  String $node3ip,
+  Optional[Tuple] $dependencies              = $cockroachdb::params::dependencies,
+  Optional[String] $description              = $cockroachdb::params::description,
+  Optional[String] $cache                    = $cockroachdb::params::cache,
+  Optional[String] $maxsqlmemory             = $cockroachdb::params::maxsqlmemory,
+  Optional[String] $timeoutstopsec           = $cockroachdb::params::timeoutstopsec,
+  Optional[String] $restart                  = $cockroachdb::params::restart,
+  Optional[String] $restartsec               = $cockroachdb::params::restartsec,
+  Optional[String] $standardoutput           = $cockroachdb::params::standardoutput,
+  Optional[String] $standarderror            = $cockroachdb::params::standarderror,
+  Optional[String] $syslogidentifier         = $cockroachdb::params::syslogidentifier,
+  Optional[String] $user                     = $cockroachdb::params::user,
+  String $node1ip                            = $cockroachdb::params::node1ip,
+  String $node2ip                            = $cockroachdb::params::node2ip,
+  String $node3ip                            = $cockroachdb::params::node3ip,
 
   #Archive
-  String $install_path,
-  String $package_name,
-  String $package_ensure,
-  String $repository_url,
-  String $archive_name,
-  String $cockroach_archive_source,
+  Optional[String] $install_path             = $cockroachdb::params::install_path,
+  Optional[String] $package_name             = $cockroachdb::params::package_name,
+  String $package_ensure                     = $cockroachdb::params::package_ensure,
+  Optional[String] $repository_url           = $cockroachdb::params::repository_url,
+  Optional[String] $archive_name             = $cockroachdb::params::archive_name,
+  Optional[String] $cockroach_archive_source = $cockroachdb::params::cockroachdb_package_source,
 
-){
+) {
   contain cockroachdb::install
   contain cockroachdb::config
   Class['::cockroachdb::install'] -> Class['::cockroachdb::config']
---
+++ manifests/params.pp
@@ -0,0 +1,31 @@
+# Class cockroachdb::params
+# =========================
+#
+# Default values for cockroachdb module
+#
+class cockroachdb::params {
+  $dependencies = ["tar", "wget"]
+  $servicepath = '/etc/systemd/system/'
+  $description = 'Cockroach Database cluster node'
+  $workingdirectory = '/var/lib/cockroach'
+  $node1ip = undef
+  $node2ip = undef
+  $node3ip = undef
+  $cache = '.25'
+  $maxsqlmemory = '.25'
+  $timeoutstopsec = '60'
+  $restart = 'always'
+  $restartsec = '10'
+  $standardoutput = 'syslog'
+  $standarderror = 'syslog'
+  $syslogidentifier = 'cockroach'
+  $user = 'cockroach'
+
+  # Archive
+  $install_path = '/usr/local/bin'
+  $package_ensure = undef
+  $package_name = 'cockroachdb'
+  $repository_url = 'https://binaries.cockroachdb.com'
+  $archive_name = "${package_name}-${cockroachdb::package_ensure}.tgz"
+  $cockroachdb_package_source = "${repository_url}/${archive_name}"
+}
