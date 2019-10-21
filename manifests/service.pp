7cdb283c25ed89422d8942aeab2458f5781c1e9a
Zackeus Bengtsson <zackeusb@stud.ntnu.no>
2019-10-21 16:05:22 +0200

    added additional_params #69

--- manifests/service.pp
+++ manifests/service.pp
@@ -8,25 +8,26 @@
 class cockroachdb::service {
 
   $cockroachdb_hash = {
-    'description'       => $cockroachdb::description,
-    'workingdirectory'  => $cockroachdb::workingdirectory,
-    'node1ip'           => $cockroachdb::node1ip,
-    'node2ip'           => $cockroachdb::node2ip,
-    'node3ip'           => $cockroachdb::node3ip,
-    'cache'             => $cockroachdb::cache,
-    'maxsqlmemory'      => $cockroachdb::maxsqlmemory,
-    'timeoutstopsec'    => $cockroachdb::timeoutstopsec,
-    'restart'           => $cockroachdb::restart,
-    'restartsec'        => $cockroachdb::restartsec,
-    'standardoutput'    => $cockroachdb::standardoutput,
-    'standarderror'     => $cockroachdb::standarderror,
-    'syslogidentifier'  => $cockroachdb::syslogidentifier,
-    'user'              => $cockroachdb::user,
+    'description'      => $cockroachdb::description,
+    'workingdirectory' => $cockroachdb::workingdirectory,
+    'node1ip'          => $cockroachdb::node1ip,
+    'node2ip'          => $cockroachdb::node2ip,
+    'node3ip'          => $cockroachdb::node3ip,
+    'cache'            => $cockroachdb::cache,
+    'maxsqlmemory'     => $cockroachdb::maxsqlmemory,
+    'timeoutstopsec'   => $cockroachdb::timeoutstopsec,
+    'restart'          => $cockroachdb::restart,
+    'restartsec'       => $cockroachdb::restartsec,
+    'standardoutput'   => $cockroachdb::standardoutput,
+    'standarderror'    => $cockroachdb::standarderror,
+    'syslogidentifier' => $cockroachdb::syslogidentifier,
+    'user'             => $cockroachdb::user,
+    'additionalparams' => $cockroachdb::additional_params,
   }
 
   file { '/etc/systemd/system/cockroachdb.service':
     ensure  => file,
-    content => epp('cockroachdb/cockroachdb.service.epp', $cockroachdb_hash ),
+    content => epp('cockroachdb/cockroachdb.service.epp', $cockroachdb_hash),
     notify  => Service['cockroachdb'],
   }
 
