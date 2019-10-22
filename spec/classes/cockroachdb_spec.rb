24065713452d1a670ac4a48c078e54a56fc9bac1
Zackeus Bengtsson <zackeusb@stud.ntnu.no>
2019-10-22 18:15:02 +0200

    updated test to pass using secure_mode param

--- spec/classes/cockroachdb_spec.rb
+++ spec/classes/cockroachdb_spec.rb
@@ -5,11 +5,10 @@ describe 'cockroachdb' do
     context "on #{os}" do
       let(:params) do
         {
-          'install_path' => '/usr/local/bin',
-          'package_ensure' => 'v19.1.5.linux-amd64',
           'node1ip' => 'db0',
           'node2ip' => 'db1',
           'node3ip' => 'db2',
+          'secure_mode' => false,
         }
       end
       let(:facts) { os_facts }
