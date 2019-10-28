c90b5e4d77ff59313ab6e6243b3f3df9bdd8c8f1
Zackeus Bengtsson <zackeusb@stud.ntnu.no>
2019-10-28 10:31:58 +0100

    work on testing

--- spec/classes/cockroachdb_spec.rb
+++ spec/classes/cockroachdb_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'cockroachdb' do
   on_supported_os.each do |os, os_facts|
-    context "on #{os}" do
+    context "on #{os} with default settings, secure mode off" do
       let(:params) do
         {
           'node1ip' => 'db0',
@@ -12,8 +12,9 @@ describe 'cockroachdb' do
         }
       end
       let(:facts) { os_facts }
-      let(:pre_condition) { 'include stdlib' }
-
+      let(:pre_condition) do
+        'include stdlib'
+      end
       it { is_expected.to compile }
       it { is_expected.to contain_file('/usr/local/bin/cockroach').with_mode('0755') }
       it { is_expected.to contain_file('/var/lib/cockroach').with('owner' => 'cockroach') }
