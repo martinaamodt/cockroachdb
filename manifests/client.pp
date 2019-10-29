12125c81188411da56a38025056db3ba2e65286f
Zackeus Bengtsson <zackeusb@stud.ntnu.no>
2019-10-29 20:11:57 +0100

    fixed client params

--- manifests/client.pp
+++ manifests/client.pp
@@ -17,25 +17,25 @@ class cockroachdb::client (
   Optional[String] $archive_name                   = $cockroachdb::params::archive_name,
   Optional[String] $cockroach_archive_source       = $cockroachdb::params::cockroachdb_package_source,
 ) inherits cockroachdb::params {
-  package { $cockroachdb::dependencies:
+  package { $dependencies:
     ensure   => installed,
     provider => apt,
-    before   => Archive[$cockroachdb::archive_name],
+    before   => Archive[$archive_name],
   }
 
-  archive { $cockroachdb::archive_name:
-    path         => "/tmp/${cockroachdb::archive_name}",
-    source       => $cockroachdb::cockroachdb_package_source,
+  archive { $archive_name:
+    path         => "/tmp/${archive_name}",
+    source       => $cockroachdb_package_source,
     extract      => true,
     extract_path => '/tmp/',
-    creates      => "${cockroachdb::install_path}/${cockroachdb::package_name}",
+    creates      => "${install_path}/${package_name}",
     cleanup      => true,
-    before       => File["${cockroachdb::install_path}/${cockroachdb::package_name}"],
+    before       => File["${install_path}/${package_name}"],
   }
 
-  file { "${cockroachdb::install_path}/${cockroachdb::package_name}":
+  file { "${install_path}/${package_name}":
     ensure => present,
-    source => "/tmp/${cockroachdb::package_name}-${cockroachdb::package_ensure}/${cockroachdb::package_name}",
+    source => "/tmp/${package_name}-${package_ensure}/${package_name}",
     mode   => '0755',
   }
 }
