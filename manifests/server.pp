ac4042493e6202ba748fd9dc8515a6db28ae7d37
Zackeus Bengtsson <zackeusb@stud.ntnu.no>
2019-10-29 18:53:35 +0100

    Removed init, moved contents to server

--- manifests/server.pp
+++ manifests/server.pp
@@ -1,10 +1,101 @@
-# @summary A short summary of the purpose of this class
 #
-# A description of what this class does
 #
-# @example
-#   include cockroachdb::server
-class cockroachdb::server (
+# @summary
+#   Installs cockroachdb as a service to run a server using the cockroach::install, cockroachdb::service
+#   and cockroach::config classes.
+#
+#
+#
+# @param servicepath
+#   Location of where the ".service" file is to be saved.
+#   If the parameter is not defined it will be saved in "/etc/systemd/system".
+#
+# @param workingdirectory
+#   Location of where the working directory will be created.
+#   If the parameter is not defined it will be created in "/var/lib/cockroach".
+#
+# @param dependencies
+#   Consists of dependencies 'tar' and 'wget' by default. Failing to include these dependencies while defining
+#   this parameter yourself may result in dependency issues.
+#
+# @param description
+#   Cockroachdb node description.
+#
+# @param cache
+#   The total size for caches, shared if there are multiple storage devices, noted with a
+#   decimal or '%'.
+#
+# @param maxsqlmemory
+#   The maximum in-memory storage capaciy available to store temprary data for SQL queries.
+#   Noted with a decimal point or '%'.
+#
+# @param timeoutstopsec
+#   Configures the time to wait for each ExecStop command for systemd.
+#
+# @param restart
+#   Defines when the service is to be restarted. Configured with always by default
+#   and will restart on every restart setting and exit cause.
+#
+# @param restartsec
+#   Defines the time to sleep before restarting the service.
+#
+# @param standardoutput
+#   Controls where the file descriptor 1 (STDOUT) of the executed processes is connected to.
+#
+# @param standarderror
+#    Controls where the file descriptor 2 (STDERR) of the executed process is connected to.
+#
+# @param syslogidentifier
+#   Sets the process name ("syslog tag") to prefix log lines sent to the logging system
+#   or the kernel log buffer.
+#
+# @param user
+#   Sets the user/group that the processes are executed as.
+#
+# @param node1ip
+#   Sets the ip-address for node one.
+#
+# @param node2ip
+#   Sets the ip-address for node two.
+#
+# @param node3ip
+#   Sets the ip-address for node three.
+#
+# @param install_path
+#   Location where the program binary file is to be located.
+#
+# @param package_name
+#   Name of the package to install.
+#
+# @param package_ensure
+#   Version number and linux distribution name.
+#
+# @param repository_url
+#   Base repository URL.
+#
+# @param archive_name
+#   Archive path of the binary file in the base repository.
+#
+# @param cockroach_archive_source
+#   Full binary package source.
+#
+# @param additional_params
+#   Additional params and flags to pass to cockroachdb on startup. Must be provided as fully valid cockroachdb flags.
+#
+# @param secure_mode
+#   If cockroachdb secure mode is enabled or not. Currently only supports insecure mode.
+#
+# @param locality
+#   Array of Hashes containing locality information for nodes.
+#
+# @param certs_dir
+#   The directory where CockroachDB can find certificates.
+# @param dns_mode
+#   Use FQDN, hostnames, IPs or custom facts to identify nodes.
+# @param dns_mode_custom
+#   Custom fact used to identify nodes. Can be any valid facter fact, like custom facts.
+#
+class cockroachdb (
   Optional[Stdlib::Absolutepath] $servicepath                         = $cockroachdb::params::servicepath,
   Optional[Stdlib::Absolutepath] $workingdirectory                    = $cockroachdb::params::workingdirectory,
   Optional[Tuple] $dependencies                                       = $cockroachdb::params::dependencies,
@@ -33,7 +124,9 @@ class cockroachdb::server (
   String $package_ensure                                              = $cockroachdb::params::package_ensure,
   Optional[String] $repository_url                                    = $cockroachdb::params::repository_url,
   Optional[String] $archive_name                                      = $cockroachdb::params::archive_name,
-  Optional[String] $cockroach_archive_source                          = $cockroachdb::params::cockroachdb_package_source,
+  Optional[String] $cockroach_archive_source                          = $cockroachdb::params::cockroachdb_package_source
+  ,
+
 ) inherits cockroachdb::params {
   contain cockroachdb::install
   contain cockroachdb::config
--- spec/classes/server_spec.rb
+++ spec/classes/server_spec.rb
@@ -1,11 +1,49 @@
 require 'spec_helper'
 
-describe 'cockroachdb::server' do
-  on_supported_os.each do |os, os_facts|
-    context "on #{os}" do
-      let(:facts) { os_facts }
+describe 'cockroachdb::server', type: :class do
+  let(:params) do
+    {
+        node1ip: 'db0',
+        node2ip: 'db1',
+        node3ip: 'db2',
+        secure_mode: false,
+    }
+  end
+  let(:node) { 'db1' }
 
+  on_supported_os.each do |os, os_facts|
+    let(:facts) { os_facts }
+
+    context "on #{os} with default settings, secure mode off" do
       it { is_expected.to compile }
+      # Verify that executable is installed
+      it { is_expected.to contain_file('/usr/local/bin/cockroach').with(mode: '0755') }
+      # Default install directory
+      it { is_expected.to contain_file('/var/lib/cockroach').with(owner: 'cockroach') }
+      # Verify that secure mode is off
+      it { is_expected.to contain_file('/etc/systemd/system/cockroachdb.service').with_content(%r{--insecure}) }
+      # Verify that hostname is inserted into service template
+      it { is_expected.to contain_file('/etc/systemd/system/cockroachdb.service').with_content(%r{--advertise-addr=db1}) }
+      it { is_expected.to contain_service('cockroachdb').with(ensure: 'running') }
+
+      it { is_expected.to contain_package('tar') }
+      it { is_expected.to contain_package('wget') }
+    end
+
+    context "on #{os} with default settings, secure mode on" do
+      let(:params) do
+        super().merge(secure_mode: true, certs_dir: '/root/certs')
+      end
+
+      it { is_expected.to contain_file('/etc/systemd/system/cockroachdb.service').with_content(%r{--certs-dir=/root/certs}) }
+    end
+
+    context "on #{os} with different version of cockroach and install path" do
+      let(:params) do
+        super().merge(package_ensure: 'v19.1.1.linux-amd64', install_path: '/root')
+      end
+
+      it { is_expected.to contain_file('/root/cockroach').with(mode: '0755') }
     end
   end
 end
