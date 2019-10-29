cf7767115778555413c8bb53685a0f1cf7a92b23
Zackeus Bengtsson <zackeusb@stud.ntnu.no>
2019-10-29 19:39:37 +0100

    updated install class to fix params

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -3,27 +3,35 @@
 # @summary
 #   This class is called from the the cockroachdb class for installation.
 #
+# @api private
 #
 class cockroachdb::server::install {
-  package { $::cockroachdb::dependencies:
+  $dependencies = $cockroachdb::dependencies
+  $archive_name = $cockroachdb::archive_name
+  $cockroachdb_package_source = $cockroachdb::cockroachdb_package_source
+  $install_path = $cockroachdb::install_path
+  $package_name = $cockroachdb::package_name
+  $package_ensure = $cockroachdb::package_ensure
+
+  package { $dependencies:
     ensure   => installed,
     provider => apt,
-    before   => Archive[$::cockroachdb::archive_name],
+    before   => Archive[$archive_name],
   }
 
-  archive { $cockroachdb::archive_name:
-    path         => "/tmp/${cockroachdb::archive_name}",
-    source       => $cockroachdb::cockroachdb_package_source,
+  archive { $archive_name:
+    path         => "/tmp/${archive_name}",
+    source       => $cockroachdb_package_source,
     extract      => true,
     extract_path => '/tmp/',
-    creates      => "${cockroachdb::install_path}/${cockroachdb::package_name}",
+    creates      => "${install_path}/${package_name}",
     cleanup      => true,
-    before       => File["${cockroachdb::install_path}/${cockroachdb::package_name}"],
+    before       => File["${install_path}/${package_name}"],
   }
 
-  file { "${cockroachdb::install_path}/${cockroachdb::package_name}":
+  file { "${install_path}/${package_name}":
     ensure => present,
-    source => "/tmp/${cockroachdb::package_name}-${cockroachdb::package_ensure}/${cockroachdb::package_name}",
+    source => "/tmp/${package_name}-${package_ensure}/${package_name}",
     mode   => '0755',
   }
 }
