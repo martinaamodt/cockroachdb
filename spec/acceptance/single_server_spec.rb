1ae37c61a617dcfe3326b4c4515a9de172da14fe
Zackeus Bengtsson <zackeusb@stud.ntnu.no>
2019-10-30 23:58:18 +0100

    Single node mode tests

--- spec/acceptance/single_server_spec.rb
+++ spec/acceptance/single_server_spec.rb
@@ -1,7 +1,35 @@
 require 'spec_helper_acceptance'
 
 describe 'cockroachdb class' do
-  describe 'default settings' do
+  describe 'default settings, secure mode off, single server mode' do
+    let(:pp) do
+      <<-MANIFEST
+          class { 'cockroachdb':
+            node1ip     => 'localhost',
+            secure_mode => false,
+          }
+      MANIFEST
+    end
+
+    it 'behaves idempotently' do
+      idempotent_apply(pp)
+    end
+
+    it 'executes' do
+      expect(cockroachdb_version).to match('v19.1.5')
+    end
+
+    describe file('/etc/systemd/system/cockroachdb.service') do
+      its(:content) { is_expected.to match %r{--listen-addr=localhost} }
+    end
+
+    describe service('cockroachdb.service') do
+      it { is_expected.to be_enabled }
+      it { is_expected.to be_running }
+    end
+  end
+
+  describe 'default settings, secure mode off, multi server mode' do
     let(:pp) do
       <<-MANIFEST
           class { 'cockroachdb':
@@ -14,12 +42,15 @@ describe 'cockroachdb class' do
     end
 
     it 'behaves idempotently' do
-      apply_manifest(pp, catch_failures: true)
-      apply_manifest(pp, catch_changes: true)
+      idempotent_apply(pp)
     end
 
     it 'executes' do
       expect(cockroachdb_version).to match('v19.1.5')
+    end
+
+    describe file('/etc/systemd/system/cockroachdb.service') do
+      its(:content) { is_expected.to match %r{--join=db0,db1,db2} }
     end
 
     describe service('cockroachdb.service') do
